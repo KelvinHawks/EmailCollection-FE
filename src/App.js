63c65ec30ecfd2826dbd39c9967e697d2edc36b1
kelvin <kevohkyme35@gmail.com>
2024-01-31 00:20:15 +0300

    Add input modal

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
   return (
     <div className="App">
       <MainNavigation />
-
       <TopHeader />
       <ImagesNav />
       <Modal
--- src/MainBody/components/ImagesNav.js
+++ src/MainBody/components/ImagesNav.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 //import ImageItem from "./ImageItem";
 import "./ImagesNav.css";
 function ImagesNav() {
+  const [showModal, setShowModal] = useState(false);
+  const openModalHandler = () => {
+    setShowModal(true);
+  };
+  const closeModalHandler = () => {
+    setShowModal(false);
+  };
   return (
     <div className="image-nav__center">
+      {showModal && (
+        <form className="submit_Modal">
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <button onClick={closeModalHandler}>Close</button>
+        </form>
+      )}
       <div className="content-page">
         <p className="main_header">Get 70% off all design books</p>
         <p className="book_description">
@@ -12,7 +29,10 @@ function ImagesNav() {
           are doing. Their vibrant social community and exciting selections
           consistently create a fun space for discussions.
         </p>
-        <button className="shop_button"> ORDER NOW</button>
+        <button className="shop_button" onClick={openModalHandler}>
+          {" "}
+          ORDER NOW
+        </button>
       </div>
     </div>
   );
